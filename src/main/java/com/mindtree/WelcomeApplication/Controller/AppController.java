a8e3b795368d664c75386266d7b85b0502723a13
Shivapriya Mogankumar <shivapriya.mogankumar2@mindtree.com>
2021-11-09 07:48:52 +0530

    first commit for welcome app

---
+++ src/main/java/com/mindtree/WelcomeApplication/Controller/AppController.java
@@ -0,0 +1,17 @@
+package com.mindtree.WelcomeApplication.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/welcome")
+public class AppController {
+
+	@RequestMapping("/page")
+	public String print(Model model)
+	{
+		model.addAttribute("mess","Welcome to SpringMVC Demo");
+		return "welcome";
+	}
+}
---
+++ src/test/java/com/mindtree/WelcomeApplication/WelcomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.WelcomeApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
